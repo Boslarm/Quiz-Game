92a06a6d6c29dacc592e6a37455a7a3c0fb233de
Jordan <jordantylerharris@gmail.com>
2022-09-05 15:06:09 -0400

    working on displaying question

--- js/logic.js
+++ js/logic.js
@@ -1,11 +1,15 @@
-// Timer
+
 var startBtn = document.getElementById("start");
 var timerEl = document.getElementById("time");
-var timeLeft = 60;
+var timeLeft = 59;
+var startScreen = document.getElementById("start-screen");
+var questionBox = document.getElementById("question-box");
 
+// Timer & Start
 startBtn.addEventListener('click', countdown); 
     function countdown() {    
         startScreen.style.display = "none"; //removes start screen
+        questionBox.style.display = "block"; //shows questions
         var timeInterval = setInterval(function () {
           timerEl.textContent = timeLeft + '';
           timeLeft--;
@@ -14,14 +18,14 @@ startBtn.addEventListener('click', countdown);
             clearInterval(timeInterval);
           } 
         }, 1000);
+        startQuiz(); //i want to start quiz after the timer starts, right?
     }
 
 
 // Questions
-var questionBox = document.getElementById("question-box");
-var questionTextEl = document.getElementById("question-title");
-var choices = document.getElementById("choices");
 
+var questionTextEl = document.getElementById("question-title");
+var choiceTextEl = document.getElementById("choices");
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
@@ -30,9 +34,17 @@ var questions = [
     },
 ];
 
-// Start
-var startBtn = document.getElementById("start");
-var startScreen = document.getElementById("start-screen");
 
-questionsBox.stlye.display = "block";
+function startQuiz() {
+    var currentQuestion = questions[questionIndex];
+    questionTextEl.textContent = currentQuestion.text;
+    choiceTextEl.innerHTML = "";
+    
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+        var choiceBtn = document.createElement("button");
 
+        choiceBtn.setAttribute("class", "btn");
+        choiceBtn.textContent = currentQuestion.choices[i];
+        choiceTextEl.appendChild(choiceBtn);
+    }; //why isnt this working?
+};
