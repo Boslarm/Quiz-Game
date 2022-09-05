11cc657cb33731a030c39985dcfe48a11792ac7c
Jordan <jordantylerharris@gmail.com>
2022-09-05 13:32:49 -0400

    got timer working and simplified js to one file; also started css

--- js/logic.js
+++ js/logic.js
@@ -1,20 +1,38 @@
-var timerEl = document.getElementById("time");
+// Timer
 var startBtn = document.getElementById("start");
+var timerEl = document.getElementById("time");
+var timeLeft = 60;
+
+startBtn.addEventListener('click', countdown); 
+    function countdown() {    
+        startScreen.style.display = "none"; //removes start screen
+        var timeInterval = setInterval(function () {
+          timerEl.textContent = timeLeft + '';
+          timeLeft--;
+      
+          if(timeLeft === 0) {
+            clearInterval(timeInterval);
+          } 
+        }, 1000);
+    }
 
 
+// Questions
+var questionBox = document.getElementById("question-box");
+var questionTextEl = document.getElementById("question-title");
+var choices = document.getElementById("choices");
 
-startBtn.addEventListener("click", countdown());
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: "alerts",
+    },
+];
 
-function countdown() {
-    var timeLeft = 90;
-  
-    var timeInterval = setInterval(function () {
-      timerEl.textContent = timeLeft + '';
-      timeLeft--;
-  
-      if(timeLeft === 0) {
-        clearInterval(timeInterval);
-      } 
-    }, 1000);
-  }
-  startBtn.addEventListener("click", countdown());
+// Start
+var startBtn = document.getElementById("start");
+var startScreen = document.getElementById("start-screen");
+
+questionsBox.stlye.display = "block";
+
